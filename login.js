b87e57654b8fd3bd9cdffff7dee7f06541d8ad84
Jose Alberto Mendoza Chavez <huero_na@hotmail.com>
2024-03-12 21:18:25 -0600

    Modificación Login

--- login.js
+++ login.js
@@ -37,3 +37,58 @@ async function iniciarSesion() {
 }
 
 document.getElementById('btnIniciarSesion').addEventListener('click', iniciarSesion);
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Enfocar el primer campo de entrada al cargar la página
+    document.getElementById('usuarioInput').focus();
+
+    // Configurar la navegación entre campos de entrada al presionar Tab
+    document.getElementById('usuarioInput').addEventListener('keydown', function(event) {
+        if (event.key === 'Tab') {
+            event.preventDefault(); // Evita el comportamiento predeterminado del Tab
+            document.getElementById('contrasenaInput').focus(); // Enfoca el campo de contraseña
+        }
+    });
+
+    // En el campo de contraseña, si Tab se presiona, va al botón de Iniciar Sesión
+    document.getElementById('contrasenaInput').addEventListener('keydown', function(event) {
+        if (event.key === 'Tab') {
+            event.preventDefault(); // Evita el comportamiento predeterminado del Tab
+            document.getElementById('btnIniciarSesion').focus(); // Enfoca el botón de Iniciar Sesión
+        }
+    });
+
+    // Enviar el formulario al presionar Enter en los campos de entrada
+    document.getElementById('usuarioInput').addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            event.preventDefault(); // Evita el comportamiento predeterminado del Enter
+            document.getElementById('btnIniciarSesion').click(); // Simula hacer clic en el botón de Iniciar Sesión
+        }
+    });
+
+    document.getElementById('contrasenaInput').addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            event.preventDefault(); // Evita el comportamiento predeterminado del Enter
+            document.getElementById('btnIniciarSesion').click(); // Simula hacer clic en el botón de Iniciar Sesión
+        }
+    });
+});
+
+// Función para mostrar u ocultar la contraseña
+function togglePassword() {
+  const passwordInput = document.getElementById('contrasenaInput');
+  const toggleButton = document.getElementById('togglePassword');
+  const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+  passwordInput.setAttribute('type', type);
+  toggleButton.src = type === 'password' ? 'iconos/ojo.png' : 'iconos/ver.png';
+}
+
+// Evento clic del botón para mostrar/ocultar la contraseña
+document.getElementById('togglePassword').addEventListener('click', togglePassword);
+
+// Evento keydown para detectar la tecla Enter
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    document.getElementById('btnIniciarSesion').click();
+  }
+});
